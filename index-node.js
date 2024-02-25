45116e36006d06fad6b46c0d25bc2eee498eb359
Nicolas Paredes <nico.d.paredes@gmail.com>
2024-02-24 23:47:42 -0600

    modified:   .gitignore 	modified:   index-node.js 	modified:   process.json

--- index-node.js
+++ index-node.js
@@ -1,47 +1,49 @@
-const express = require("express");
+const express = require('express'),
 app = express();
 
-const cors = requre("cors")
-var url = require("url")
-var dt = require("./date-time")
+const cors = require("cors");
+
+var url = require('url');
+var dt = require('./date-time');
 
 const port = process.env.PORT || 3000
-const majorVersion =1
-const minorVersion=3
+const majorVersion = 1
+const minorVersion = 3
 
+app.use(cors({ origin: '*' }))
 
-app.use(express.static(__dirname + '/static'))
-app.use(cors({ orgin: "*"}))
-
-app.get("/about", (request, responce) => {
-	console.log('Calling "/about" on nodejs server. ')
-	responce.type('text/plain')
-	responce.send("This is a website that utilizes server-side nodejs to implement a dice rolling app.")
-})
-
-app.get("/dice", (request, response) =>{
-	console.log('Calling "/dice" on the node.js server.')
-	let die = Math.floor(Math.random()*6)+1
-	response.type('text/plain');
-	response.send(die);
-})
-
-app.use((request, responce)=>{
-	responce.type("text/plain")
-	responce.status(404)
-	responce.send("404 - not found")
-})
-
-app.use((err, request,next)=>{
-	console.error(err.message)
+app.get('/about', (request, response) => {
+	console.log('Calling "/about" on the Node.js server.')
 	response.type('text/plain')
-	responce.status(500)
-	responce.send("500 - service error")
-
+	response.send('This is a website that utilizes server-side node.js to implement a dice rolling application.')
 })
 
-app.listen(port, () => console.log(
-	'Express started at \"http://localhost:${port}"\n' +
-	'press Ctrl-C to terminate.')
-)
+app.get('/generateNum1To6', (request, response) => {
+    console.log('Calling "/generateNum1To6" on the Node.js server.')
+    
+    let die = Math.floor(Math.random() * 6) + 1;
+
+    response.type('text/plain');
+    response.send(die.toString());
+})
+
+// Custom 404 page.
+app.use((request, response) => {
+    response.type('text/plain')
+    response.status(404)
+    response.send('404 - Not Found')
+  })
+  
+  // Custom 500 page.
+  app.use((err, request, response, next) => {
+    console.error(err.message)
+    response.type('text/plain')
+    response.status(500)
+    response.send('500 - Server Error')
+  })
+  
+  app.listen(port, () => console.log(
+    `Express started at \"http://localhost:${port}\"\n` +
+    `press Ctrl-C to terminate.`)
+  )
 	
